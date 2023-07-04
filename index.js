ea9403479203dfbc794a21e24e7afd847d0f7d42
Jazim hammad <90127535+jazim-hammad@users.noreply.github.com>
2023-07-05 01:25:48 +0400

    Add files via upload

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 function sendMail() {
   var params = {
     name: document.getElementById("name").value,
+    people: document.getElementById("people").value,
   };
 
   const serviceID = "service_jm6gai3";
@@ -10,8 +11,11 @@ function sendMail() {
     .send(serviceID, templateID, params)
     .then((res) => {
       document.getElementById("name").value = "";
+      document.getElementById("people").value = "";
       console.log(res);
-      alert("Your message sent successfully!!");
+      alert(
+        "Your Response is Recorded! \n Looking forward to see you at our wedding ~ Aziz & Aleeza"
+      );
     })
     .catch((err) => console.log(err));
 }
